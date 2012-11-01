74b8beb762a56963cad4a36a38231cf90b9a1bd4
Matt Stratton <matt.stratton@gmail.com>
2012-11-01 18:18:20 -0500

    Fixed layout of blog page (centered)

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<div id="content" class="widecolumn">
+	<div id="content" class="blogwidecolumn">
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<!-- This is each post in the result-->
@@ -25,19 +25,19 @@
 						<div class="blogmeta">
 						<span class="timestamp"><?php the_time(__('F jS, Y', 'notesblog')) ?> @ <?php the_time(); ?></span>
 						<?php if (comments_open()) : ?><span class="comments"><?php comments_popup_link(__('0 <span>comments</span>', 'notesblog'), __('1 <span>comment</span>', 'notesblog'), __('% <span>comments</span>', 'notesblog'), '', 'Comments are closed'); ?></span><?php endif; ?>
-						
+
 					</div>
 				<?php if (is_single() || is_page()) { edit_post_link(__('Edit', 'notesblog'), '<p class="admin">Admin: ', '</p>'); wp_link_pages('before=<p class="pagelink">' . __('Pages:', 'notesblog') .' &after=</p>'); } ?>
 			</div>
 			<?php if (is_single()) {comments_template('', true);} ?>
 
 		<?php endwhile; ?>
-		
+
 			<div class="nav widecolumn">
 				<div class="left"><?php next_posts_link(__('Read previous entries', 'notesblog')) ?></div>
 				<div class="right"><?php previous_posts_link(__('Read more recent entries', 'notesblog')) ?></div>
 			</div>
-		
+
 	<?php else : ?>
 	<?php endif; ?>
 
